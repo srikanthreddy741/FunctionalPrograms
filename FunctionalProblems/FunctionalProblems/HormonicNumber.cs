ace70e968131256dc870065c1e59683093023a98
srikanthreddy741 <bandarisrikanth741@gmail.com>
2022-07-26 22:51:44 +0530

    Hormonic Number

---
+++ FunctionalProblems/FunctionalProblems/HormonicNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProblems
+{
+    public class HormonicNumber
+    {
+        public static void HarmonicNumberSeries()
+        {
+            Console.WriteLine("Enter number ");
+
+            int num;
+            num = Convert.ToInt32(Console.ReadLine());
+
+            double sum = 0;
+
+            for (double count = 1; count <= num; count++)
+            {
+                Console.WriteLine("1/{0} + ", count);
+                sum = sum + 1 / count;
+            }
+            Console.WriteLine("\n The sum of {0} series is {1} ", num, sum);
+            Console.ReadLine(); 
+        }
+    }
+}
+
--- FunctionalProblems/FunctionalProblems/Program.cs
+++ FunctionalProblems/FunctionalProblems/Program.cs
@@ -1,19 +1,20 @@
 ﻿using FunctionalProblems;
 using System;
 
-namespace FunctionalPrograms
+namespace FunctionalProblems
 {
    public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("wellcome To Powerof2");
-            
-            Power.PowerOf2();
+            Console.WriteLine("wellcome To HormonicNumber");
 
+            HormonicNumber.HarmonicNumberSeries();
+
+            //Power.PowerOf2();
             // LeapYear Leap = new LeapYear();
             //Leap.Year();
-            
+
         }
     }
 }
